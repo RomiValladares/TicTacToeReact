21cd0cabeef9605a2830f9a5a5561f9772f6e562
Romina V <valladaresromina@gmail.com>
2026-05-19 11:26:40 -0600

    Add Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { TicTacToe } from './features/game/TicTacToe';
-import { TicTacToePreview } from './features/game/TicTacToePreview';
 
 function App() {
   return <TicTacToe />;
-  // return <TicTacToePreview />;
 }
 
 export default App;
